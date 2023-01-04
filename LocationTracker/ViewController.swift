c09fdbf4eec66bd75bacd42a711ee319b7b4f026
Edgar Hakobyan <ehakobyan94@gmail.com>
2023-01-04 23:20:21 +0400

    Initial Commit

---
+++ LocationTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationTracker
+//
+//  Created by Edgar on 04.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
